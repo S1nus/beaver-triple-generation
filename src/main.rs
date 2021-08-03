b6ebdda3d591a8ca6783732913d85bd1e2f8e8c1
Connor O'Hara <connor@s1nus.com>
2021-08-03 16:37:48 -0400

    this took me way to long to figure out... lol

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,14 @@ fn main() {
     // w = xy = (x1+x2)*(y1+y2)
     
     //x = 62
-    let x1 = 27;
-    let x2 = 35;
+    let x1 = 9;
+    let x2 = 8;
 
     //y = 97
-    let y1 = 71;
-    let y2 = 26;
+    let y1 = 3;
+    let y2 = 4;
     
-    // w should be 6014
+    // w should be 119
 
     let x1_enc = Paillier::encrypt(&ek, x1);
     let y1_enc = Paillier::encrypt(&ek, y1);
@@ -42,9 +42,11 @@ fn main() {
 
     // player 2 computes W2
     let w2 = (((x2 * y2)%7757) - r) % 7757;
+    let w2_shares = tss.share(w2 as i64);
 
     // player 1 computes w1
     let w1 = (((x1 * y1)%7757) + Paillier::decrypt(&dk, t))%7757;
+    let w1_shares = tss.share(w1 as i64);
 
     println!("w1: {} w2: {}", w1, w2);
 
@@ -56,7 +58,9 @@ fn main() {
     // The players wish to compute C = A*B without revealing A or B
     
     let a = 7;
+    let a_shares = tss.share(a);
     let b = 2;
+    let b_shares = tss.share(b);
 
     let a_shares = tss.share(a);
     let b_shares = tss.share(b);
@@ -70,31 +74,40 @@ fn main() {
     let w1_shares = tss.share(w1.try_into().unwrap());
     let w2_shares = tss.share(w2.try_into().unwrap());
 
-    // d = a - x
-    let d0 = a_shares[0] - (x1_shares[0] + x2_shares[0])%7757;
-    let d1 = a_shares[1] - (x1_shares[1] + x2_shares[1])%7757;
-    // reveal d
-    let d_revealed = tss.reconstruct(&[0,1], &[d0, d1]);
-    let d_actual = a - (x1 as i64 + x2 as i64);
-    println!("d: {} should be: {}", d_revealed, d_actual);
+    println!("A: {}", a);
+    println!("B: {}", b);
 
-    // e = b - y
-    let e0 = b_shares[0] - (y1_shares[0] + y2_shares[0])%7757;
-    let e1 = b_shares[1] - (y1_shares[1] + y2_shares[1])%7757;
-    let e_revealed = tss.reconstruct(&[0,1], &[e0, e1]);
-    let e_actual = b - (y1 as i64 + y2 as i64);
-    println!("e: {} should be: {}", e_revealed, e_actual);
+    let d = (b - (x1 as i64 + x2 as i64)+tss.prime)%tss.prime;
+    let e = (a - (y1 as i64 + y2 as i64)+tss.prime)%tss.prime;
+    println!("d: {} e: {}", d, e);
 
-    // then [z] = [w] + [x]*e + [y]*d -e*d
-    // but apparently, only one party adds e*d to the shares...
-    
-    let z0 = (w1_shares[0]+w2_shares[0])%7757 as i64 + (( (x1_shares[0]+x2_shares[0])%7757 * e_revealed ) % 7757) +
-        (( (y1_shares[0] + y2_shares[0])%7757 * d_revealed ) % 7757)
-        - ((e_revealed*d_revealed)%7757);
-    let z1 = (w1_shares[1]+w2_shares[1])%7757 as i64 + (( (x1_shares[1]+x2_shares[1])%7757 * e_revealed ) % 7757) +
-        (( (y1_shares[1] + y2_shares[1])%7757 * d_revealed ) % 7757);
+    let d_0 = (b_shares[0] - (x1_shares[0] +x2_shares[0])%tss.prime)%tss.prime;
+    let d_1 = (b_shares[1] - (x1_shares[1] +x2_shares[1])%tss.prime)%tss.prime;
+    let d_revealed = (tss.reconstruct(&[0,1], &[d_0, d_1])+tss.prime)%tss.prime;
+    println!("d_revealed: {}", d_revealed);
 
-    let z_revealed = tss.reconstruct(&[0,1], &[z0, z1]);
-    let z_actual = a*b;
-    println!("z_revealed: {} z_actual: {}", z_revealed, z_actual);
+    let e_0 = (a_shares[0] - (y1_shares[0] +y2_shares[0])%tss.prime)%tss.prime;
+    let e_1 = (a_shares[1] - (y1_shares[1] +y2_shares[1])%tss.prime)%tss.prime;
+    let e_revealed = (tss.reconstruct(&[0,1], &[e_0, e_1])+tss.prime)%tss.prime;
+    println!("e_revealed: {}", e_revealed);
+
+    let w = w1 + w2;
+    println!("w: {}", w);
+    let w0 = w1_shares[0] + w2_shares[0];
+    let w1 = w1_shares[1] + w2_shares[1];
+    let w_revealed = (tss.reconstruct(&[0,1], &[w0, w1])+tss.prime)%tss.prime;
+    println!("W revealed: {}", w_revealed);
+
+    let be_0 = (b_shares[0] * e_revealed)%tss.prime;
+    let be_1 = (b_shares[1] * e_revealed)%tss.prime;
+
+    let ad_0 = (a_shares[0] * d_revealed)%tss.prime;
+    let ad_1 = (a_shares[1] * d_revealed)%tss.prime;
+
+    let z0 = ((w1_shares[0] + w2_shares[0])%tss.prime + be_0 + ad_0 - (e_revealed*d_revealed)%tss.prime)%tss.prime;
+    let z1 = ((w1_shares[1] + w2_shares[1])%tss.prime + be_1 + ad_1)%tss.prime;
+
+    let z_revealed = tss.reconstruct(&[0,1], &[z0,z1]);
+    println!("z: {}", z_revealed);
+
 }
